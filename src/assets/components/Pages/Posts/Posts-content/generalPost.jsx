3aaff346385c2bad9bb88276e4e3e2c2486a9fc1
Alexanders91107 <Alexanders91107@gmail.com>
2025-09-14 20:44:24 -0400

    Worked on posts page

--- src/assets/components/Pages/Posts/Posts-content/generalPost.jsx
+++ src/assets/components/Pages/Posts/Posts-content/generalPost.jsx
@@ -4,7 +4,20 @@ import './css/generalPost.css';
 function GeneralPost(){
     return (
         <div className="general-post-container">
-            hello
+            <div className = "general-post-title"> INFORMATION </div>
+            <div className = "general-post-subtitle"> Date: 09/10/25 </div>
+            <div className = "general-post-content">
+                Hello, this post is just general information about this page and how it will function. This page is basically just for me; I can put literally whatever I want on it. I feel like it will probably just be me talking about my different projects, but I’m sure I’ll use this to rant about different things from time to time.. 
+                <br/><br/>
+                Anyways, as you can see below: 
+                <br/><br/><br/><br/>
+                This page will not just be blobs of text; I’ll put pictures, videos, and other things. These pages are just their React files, so I can do anything with them. 
+                <br/><br/>
+                <div className = "general-post-image-container">
+                </div>
+                Anyways, that's it. Bye!
+                <br/><br/>
+            </div>
         </div>
     );
 }
--- src/assets/components/Pages/Posts/Posts-content/post1.jsx
+++ src/assets/components/Pages/Posts/Posts-content/post1.jsx
@@ -3,8 +3,19 @@ import './css/generalPost.css';
 
 function Post1(){
     return (
-        <div className="general-post-container">
-            post1
+        <div className="general-post-container"
+            style = {{background: "linear-gradient(135deg, #228339ff 0%, #065321ff 100%)"}}
+        >
+            <div className = "general-post-title"> SPOTIFY VIEWER </div>
+            <div className = "general-post-subtitle"> Date: 09/11/25 </div>
+            <div className = "general-post-content">
+                Hello, this post is about the Spotify Viewer project that I did a couple of months ago. This project was a mess; it was COMPLETELY changed from what it was initially supposed to be. I initially was planning on using a person's liked songs to create a network of songs, separate the songs into groups, and display other interesting information about their music taste. Anyways, I had the Spotify API and the network display set up, and then like HALF of the Spotify API was depreciated, and unfortunately, that included like literally everything I needed for the project. So, I still just scrapped the project and made a site where you can just view your recent listening trends for different time periods. 
+                <br/><br/>
+                <div className = "general-post-image-container">
+                </div>
+                Anyways, that's it. Bye!
+                <br/><br/>
+            </div>
         </div>
     );
 }
