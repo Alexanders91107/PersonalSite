20d33c5f950c98bb5eafd661be8757f020e12068
Alexanders91107 <Alexanders91107@gmail.com>
2025-10-08 20:17:14 -0400

    changed centering for taskbar

--- src/assets/components/Taskbar.jsx
+++ src/assets/components/Taskbar.jsx
@@ -18,24 +18,26 @@ function Taskbar({clickFunction, hoverFunction, style}) {
 
 
   return (
-    <div 
-        className="taskbar"
-        style = {style}
-        onMouseLeave = {() => hoverFunction(false)} // Hide taskbar when mouse leaves
-    >
-        <div className = "buttons-container">
-            <Button clickFunction={clickFunction} contentType={"about"} logo={aboutSVG} />
-            <Button clickFunction={clickFunction} contentType={"projects"} logo={projectsSVG} />
-            <Button clickFunction={clickFunction} contentType={"posts"} logo={postsSVG} />
-            <div className = "game-button"><Button clickFunction={clickFunction} contentType={"game"} logo={gameSVG} /></div>
-        </div>
+    <div className="taskbar-container">
+        <div 
+            className="taskbar"
+            style = {style}
+            onMouseLeave = {() => hoverFunction(false)} // Hide taskbar when mouse leaves
+        >
+            <div className = "buttons-container">
+                <Button clickFunction={clickFunction} contentType={"about"} logo={aboutSVG} />
+                <Button clickFunction={clickFunction} contentType={"projects"} logo={projectsSVG} />
+                <Button clickFunction={clickFunction} contentType={"posts"} logo={postsSVG} />
+                <div className = "game-button"><Button clickFunction={clickFunction} contentType={"game"} logo={gameSVG} /></div>
+            </div>
 
-        <div className = "time-date-container">
-            {/* Format the date to display time and date strings */}
+            <div className = "time-date-container">
+                {/* Format the date to display time and date strings */}
 
-            {/* empty array in the timeString tells it to change format on browser default */}
-            <div className="time">{currentDate.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</div>
-            <div className="date">{currentDate.toLocaleDateString()}</div>
+                {/* empty array in the timeString tells it to change format on browser default */}
+                <div className="time">{currentDate.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</div>
+                <div className="date">{currentDate.toLocaleDateString()}</div>
+            </div>
         </div>
     </div>
   );
